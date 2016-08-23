dcd50a997cf7c8021d24d37277bcbc23e70f4370
rodrigoramos94 <rramosparaiso@gmail.com>
2016-08-23 12:34:49 +0200

    app port

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ import path = require('path');
 var app = express();
 
 // all environments
-app.set('port', 8080);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
